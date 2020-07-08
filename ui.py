d6b11a3a46a38e9364954f89c533c8c0084e484a
Penultimate-Panacea <rian+github@fantozzi.org>
2020-07-08 14:59:25 -0400

    Added UWP Manual Entry

--- ui.py
+++ ui.py
@@ -16,7 +16,21 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setWindowTitle('World Crumbler')
         self.setFixedSize(1000, 640)
         self.setupUi(self)
+        self.secondSurveyUwp = ['X', '0', '0', '0', '0', '0', '0', '0']
+        self.hardTimesUwp = ['X', '0', '0', '0', '0', '0', '0', '0']
+        self.ManualInputPushButton.clicked.connect(self.manual_uwp)
+        #self.subsector_status =
 
+    def manual_uwp(self):
+        self.secondSurveyUwp[0] = self.OriginalStarportInput.currentText()
+        self.secondSurveyUwp[1] = self.OriginalSizeInput.currentText()
+        self.secondSurveyUwp[2] = self.OriginalAtmosphereInput.currentText()
+        self.secondSurveyUwp[3] = self.OriginalHydrosphereInput.currentText()
+        self.secondSurveyUwp[4] = self.OriginalPopulationInput.currentText()
+        self.secondSurveyUwp[5] = self.OriginalGovernmentInput.currentText()
+        self.secondSurveyUwp[6] = self.OriginalLawInput.currentText()
+        self.secondSurveyUwp[7] = self.OriginalTechInput.currentText()
+        print(self.secondSurveyUwp)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(argv)
