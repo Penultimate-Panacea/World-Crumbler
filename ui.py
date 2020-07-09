b147ceda9f7db91230f96bd313dc00372557c635
Penultimate-Panacea <rian+github@fantozzi.org>
2020-07-09 12:42:34 -0400

    Added Crumble Stage connectors

--- ui.py
+++ ui.py
@@ -16,10 +16,25 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setWindowTitle('World Crumbler')
         self.setFixedSize(1000, 640)
         self.setupUi(self)
+        self.sectorName = "Test Sector Name"
+        self.worldName = "Test World Name"
+        self.hexagon = 9999
+        self.dice_seed = None
         self.secondSurveyUwp = ['X', '0', '0', '0', '0', '0', '0', '0']
         self.hardTimesUwp = ['X', '0', '0', '0', '0', '0', '0', '0']
         self.ManualInputPushButton.clicked.connect(self.manual_uwp)
         self.TravellerMapGetPlanet.clicked.connect(self.api_uwp)
+        self.CrumbleStage_1.clicked.connect(self.crumble1)
+        self.CrumbleStage_2.clicked.connect(self.crumble2)
+        self.CrumbleStage_3.clicked.connect(self.crumble3)
+        self.CrumbleStage_4.clicked.connect(self.crumble4)
+        self.CrumbleStage_5.clicked.connect(self.crumble5)
+        self.CrumbleStage_6.clicked.connect(self.crumble6)
+        self.CrumbleStage_7.clicked.connect(self.crumble7)
+        self.CrumbleStage_8.clicked.connect(self.crumble8)
+        self.CrumbleStage_9.clicked.connect(self.crumble9)
+        self.CrumbleStage_10.clicked.connect(self.crumble10)
+        self.AutoCrumble.clicked.connect(self.auto_crumble)
 
     def manual_uwp(self):
         self.secondSurveyUwp[0] = self.OriginalStarportInput.currentText()
@@ -30,12 +45,15 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.secondSurveyUwp[5] = self.OriginalGovernmentInput.currentText()
         self.secondSurveyUwp[6] = self.OriginalLawInput.currentText()
         self.secondSurveyUwp[7] = self.OriginalTechInput.currentText()
+        self.sectorName = self.SectorNameInput.currentText()
+        self.worldName = self.PlanetManualInput.text()
+        self.dice_seed = self.sectorName+self.worldName
         self.hardTimesUwp = self.secondSurveyUwp
 
     def api_uwp(self):
-        sector = self.TravellerMapSector.currentText()
-        hexagon = self.TravellerMapHex.text()
-        world = tmap_gets.get_json(sector, hexagon)
+        self.sectorName = self.TravellerMapSector.currentText()
+        self.hexagon = self.TravellerMapHex.text()  # TODO hex validator
+        world = tmap_gets.get_json(self.sectorName, self.hexagon)
         uwp_returned = world["WorldUwp"]
         uwp_string = uwp_returned.replace('-', '')
         i = 0
@@ -46,6 +64,7 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         world_string = "Sector:" + world["SectorName"] + "\nSubsector:" + world["SubsectorName"] + "\nWorld:" + \
                        world["WorldName"] + "\nUWP:" + uwp_returned
         self.ReturnedAPILabel.setText(world_string)
+        self.dice_seed = self.sectorName + self.worldName
         self.hardTimesUwp = self.secondSurveyUwp
 
 
