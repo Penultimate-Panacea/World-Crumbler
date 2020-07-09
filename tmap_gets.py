7de05a361c776e0bd27a4471241b85edcc14afc9
Penultimate-Panacea <rian+github@fantozzi.org>
2020-07-09 11:33:45 -0400

    Added todo items

--- tmap_gets.py
+++ tmap_gets.py
@@ -7,3 +7,6 @@ def get_json(sector, hexagon):
     with urlopen("https://travellermap.com/api/credits?sector=%s&hex=%s" % (quote(sector), hexagon)) as url:
         json = loads(url.read().decode())
     return json
+
+# TODO Error Handling
+# TODO Make into full library?
