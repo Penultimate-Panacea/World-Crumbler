7260ebe60df98c07cdcac79c7d7f1c4d54cf8b6c
Penultimate-Panacea <rian+github@fantozzi.org>
2020-07-10 10:44:06 -0400

    Connected Stage 6b and fixed some minor bugs

--- ui.py
+++ ui.py
@@ -36,8 +36,9 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.CrumbleWidget.setDisabled(True)
         self.CrumbleStage_1a.clicked.connect(self.crumble1a)
         self.CrumbleStage_1b.clicked.connect(self.crumble1b)
-        self.CrumbeStage_3.clicked.connect(self.crumble3)
+        self.CrumbleStage_3.clicked.connect(self.crumble3)
         self.CrumbleStage_6a.clicked.connect(self.crumble6a)
+        self.CrumbleStage_6b.clicked.connect(self.crumble6b)
         self.degrees_of_change_dict = {1: 0, 2: 0, 3: 0, 4: 0, 5: -1, 6: -1, 7: -2, 8: -2, 9: -3, 10: -3, 11: -4,
                                        12: -4, 13: -5, 14: -5, 15: -6, 16: -6, 17: -7, 18: -7, 19: -8, 20: -8, 21: -9,
                                        22: -9, 23: -10, 24: -10, 25: -11, 26: -11, 27: -12, 28: -12, 29: -13, 30: -13}
@@ -379,8 +380,9 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         c_decrease_dict = {'D': 1, 'E': 2, 'X': 3}
         d_decrease_dict = {'E': 1, 'X': 2}
         e_decrease_dict = {'X': 1}
+        x_decrease_dict = {'X': 0}
         starport_decrease_dict = {'A': a_decrease_dict, 'B': b_decrease_dict, 'C': c_decrease_dict,
-                                  'D': d_decrease_dict, 'E': e_decrease_dict}
+                                  'D': d_decrease_dict, 'E': e_decrease_dict, 'X': x_decrease_dict}
         dicemod += starport_decrease_dict[self.secondSurveyUwp[0]][self.hardTimesUwp[0]]
         crumble_roll = self.dice.roll_1d6() + dicemod
         reduction = self.degrees_of_change_dict[crumble_roll]
